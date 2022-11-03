e60d287a2e009b7f152209432af032717c233aab
Yadong Zhang <zhyd007@gmail.com>
2022-11-03 14:29:42 +0800

    feat: integrated Superfluid service.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,21 @@ import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import { ChangeEvent, useState } from 'react';
 
+import { utils } from "ethers";
+
+import { Framework } from "@superfluid-finance/sdk-core";
+
+import { useAccount, useNetwork, useProvider, useSigner } from 'wagmi'
+
+
 export default function Home() {
+  const { chain } = useNetwork();
+  const provider = useProvider();
+  const { address: sender } = useAccount();
+  const { data: signer } = useSigner();
+
+  const [isLoading, setIsLoading] = useState(false);
+
   const [recipient, setRecipient] = useState('');
   const [amount, setAmount] = useState('');
 
@@ -20,6 +34,42 @@ export default function Home() {
       return;
     }
     setAmount(value);
+  }
+
+  const sponsor = async () => {
+    if (!chain || !sender || !signer) {
+      return;
+    }
+    setIsLoading(true);
+
+    try {
+      const paymentToken = process.env.NODE_ENV === "development" ? "fDAIx" : "DAIx";
+      const monthlyAmount = utils.parseEther(amount);
+      const flowRate = monthlyAmount.div(60 * 60 * 24 * 30).toString();
+
+      const receiver = utils.getAddress(recipient);
+
+      const sf = await Framework.create({
+        chainId: chain.id,
+        provider,
+      });
+
+      const superTokenCls = await sf.loadSuperToken(paymentToken)
+      const superToken = superTokenCls.address;
+
+      const createFlowOperation = sf.cfaV1.createFlow({
+        flowRate,
+        sender,
+        receiver,
+        superToken,
+      });
+
+      await createFlowOperation.exec(signer);
+    } catch (e) {
+      console.error(e);
+    }
+
+    setIsLoading(false);
   }
 
   return (
@@ -37,7 +87,7 @@ export default function Home() {
           <TextField
             required
             id="outlined-required"
-            label="Wallet Address or ENS"
+            label="Wallet Address"
             value={recipient}
             onChange={(evt) => { setRecipient(evt.target.value.trim()) }}
           />
@@ -55,7 +105,8 @@ export default function Home() {
         <Button
           variant="contained"
           endIcon={<SendIcon />}
-          onClick={() => {}}
+          disabled={isLoading}
+          onClick={sponsor}
         >
           Send
       </Button>
