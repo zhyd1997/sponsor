d2b87f0545d021f7822a63b80abd9cbdb8bf154b
Yadong Zhang <zhyd007@gmail.com>
2022-11-04 15:45:41 +0800

    fix: SEO (#11)

---
+++ components/Meta/Meta.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export const Meta = () => {
+  return (
+    <Head>
+      <meta name="description" content="Sponsor devs on Github using crypto." />
+      <meta name="viewport" content="width=device-width" />
+      <meta charSet="utf-8" />
+      <title>Sponsor</title>
+    </Head>
+  );
+};
