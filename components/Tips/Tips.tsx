bf8fa4fc9b51a1ef8489db662878d7903b824035
Yadong Zhang <zhyd007@gmail.com>
2022-11-05 18:10:34 +0800

    fix: used Alert in Tips component.

--- components/Tips/Tips.tsx
+++ components/Tips/Tips.tsx
@@ -1,17 +1,18 @@
+import { Alert } from "@/components/Alert";
+
 export const Tips = () => {
   return (
-    <p>
+    <Alert severity="error">
       No enough <b>DAIx</b>?
       <br />
       Wrap some <b>DAI</b> on the&nbsp;
       <a
         href="https://app.superfluid.finance/wrap?upgrade"
         target="_blank"
-        style={{ background: "white", padding: "8px", borderRadius: "8px" }}
       >
         Superfluid
       </a>
       &nbsp;page and then back.
-    </p>
+    </Alert>
   );
 };
